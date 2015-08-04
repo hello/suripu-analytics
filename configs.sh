e58467ef69c4c26cf3ec7b39e96cdf743ea628a3
Josef Norgan <josef@sayhello.com>
2015-08-04 10:37:07 -0700

    Modifying project for Travis build

--- configs.sh
+++ configs.sh
@@ -8,9 +8,6 @@ fi
 
 VERSION=$1
 
-# pardon the copy pasta but I don't know shit about bash
-
-# prod
-
 s3cmd put configs/analytics.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-analytics/$VERSION/analytics.prod.yml
+s3cmd put configs/analytics.staging.yml s3://hello-deploy/configs/com/hello/suripu/suripu-analytics/$VERSION/analytics.staging.yml
 
