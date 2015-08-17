1cd68c6a3b64cde261f6f5843830d99dafb20079
Tim Bart <tim@sayhello.com>
2015-08-17 12:45:27 -0700

    Remove debug statements

--- src/main/java/com/hello/suripu/analytics/AnalyticsProcessor.java
+++ src/main/java/com/hello/suripu/analytics/AnalyticsProcessor.java
@@ -1,8 +1,5 @@
 package com.hello.suripu.analytics;
 
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.annotation.Timed;
 import com.hello.suripu.analytics.cli.CreateDynamoDBTables;
 import com.hello.suripu.analytics.configuration.AnalyticsConfiguration;
 import com.hello.suripu.analytics.processors.PillStatsCommand;
@@ -12,11 +9,7 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.joda.time.DateTimeZone;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import java.util.TimeZone;
-
-import static com.codahale.metrics.MetricRegistry.name;
 
 /**
  * A dropwizard application that essentially mimics the same/similar structure as the
@@ -45,29 +38,9 @@ public class AnalyticsProcessor extends Application<AnalyticsConfiguration>
         bootstrap.addCommand(new CreateDynamoDBTables());
     }
 
-    @Path("/")
-    public static class TestResource {
-        private final MetricRegistry metrics;
-        private final Meter meter;
-
-        public TestResource(final MetricRegistry metricRegistry) {
-            metrics = metricRegistry;
-            meter = metrics.meter(name(TestResource.class, "whatever"));
-        }
-
-
-
-        @GET
-        @Timed
-        public String something() {
-            meter.mark(1);
-            return "something";
-        }
-    }
-
     @Override
     public void run(AnalyticsConfiguration configuration,
                     Environment environment) {
-        environment.jersey().register(new TestResource(environment.metrics()));
+        // DO NOTHING HERE
     }
 }
